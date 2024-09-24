e8d4ea62b4674af9d6d3a5b2c51e825916085b3e
Park jaemin <hungryshark4292@gmail.com>
2024-09-24 22:36:18 +0900

    database :: 연결

--- app/database.py
+++ app/database.py
@@ -11,8 +11,11 @@ SessionLocal = sessionmaker(autoflush= False, autocommit = False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  except:
-    db.close()
+    db = SessionLocal()  # 세션 생성
+    try:
+        yield db
+    except Exception as e:
+        print(f"Database error: {e}")  # 로그 추가
+        raise  # 예외를 다시 발생시킴
+    finally:
+        db.close()
