2b137b666b6ecaefffa43cb0a891c1f2ed089914
Park jaemin <hungryshark4292@gmail.com>
2024-09-25 17:13:15 +0900

    notification

---
+++ notification/database.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://yabbi:12341234@43.203.215.239:5432/dcz"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autoflush= False, autocommit = False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()  
+    try:
+        yield db
+    except Exception as e:
+        print(f"Database error: {e}")  
+        raise  
+    finally:
+        db.close()
