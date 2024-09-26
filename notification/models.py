bc6ed1aabd883cca7cc96c4cf2d2aaf5dedf9203
Park jaemin <hungryshark4292@gmail.com>
2024-09-26 14:09:46 +0900

    notification-models

--- notification/models.py
+++ notification/models.py
@@ -1,11 +1,39 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Date, Enum
+from notification.database import Base
+from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
+import uuid
+from sqlalchemy.orm import relationship
+import enum
+
+class Role(enum.Enum):
+    STU = "STU"
+    ADMIN = "ADMIN"
+
+class User(Base):
+    __tablename__ = "user_info"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    xquare_id = Column(UUID(as_uuid=True), nullable=False)
+    account_id = Column(String(40), nullable=False, unique=True)
+    name = Column(String(10), nullable=False)
+    grade = Column(Integer, nullable=False)
+    class_num = Column(Integer, nullable=False)
+    num = Column(Integer, nullable=False)
+    birth_day = Column(Date, nullable=False)
+    device_token = Column(String, nullable=True)
+    profile = Column(String, nullable=True)
+    role = Column(Enum(Role), nullable=False)
+    notifications = relationship("Notification", back_populates="author")  # 관계 설정
 
 
 
-class notification(Base):
-  __tablename__ = "notification"
-  
-  id = Column(Integer)
-  title = Column
+class Notification(Base):
+    __tablename__ = "notification"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(String)
+    date = Column(DateTime, nullable=False , default=datetime.now)
+    author_id = Column(UUID(as_uuid=True), ForeignKey("user_info.id"))  
+    author = relationship("User", back_populates="notifications")  
