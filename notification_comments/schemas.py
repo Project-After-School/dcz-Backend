5b900e7ac53bca846d38f7d01af1f5106daedd31
Park jaemin <hungryshark4292@gmail.com>
2024-10-07 19:28:45 +0900

    comments_schemas

---
+++ notification_comments/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+from datetime import datetime
+
+class CommentsBase(BaseModel):
+  content : str
+  
+class CreateComments(CommentsBase):
+  pass
+
+class Comments(CommentsBase):
+  id : int
+  author_id = UUID
+  notification_id = int
+  data = datetime
+  
+  class Config:
+    from_attributes = True
+  
