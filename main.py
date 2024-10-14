c981dfce1629d29165a52ae5b782ec351caf1ba4
Park jaemin <hungryshark4292@gmail.com>
2024-10-14 23:20:08 +0900

    main 수정

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from notification import router as notification_router
 from notification_comments import router as comments_router
 from fastapi.middleware.cors import CORSMiddleware
 from admin_signin import admin_router
+from user_mypage import router as user_mypage_router
 
 app = FastAPI()
 
@@ -19,6 +20,7 @@ app.include_router(user_router.router, tags=["유저"]) # 유저
 app.include_router(notification_router.router, tags=["공지"]) # 공지
 app.include_router(comments_router.router, tags=['공지사항 댓글'])
 app.include_router(admin_router.router, tags=['어드민 회원가입']) # 어드민 회원가입
+app.include_router(user_mypage_router.router, tags=['유저 마이페이지'])
 
 if __name__ == "__main__":
   import uvicorn
