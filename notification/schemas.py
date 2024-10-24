5c680186ce57db3242eaed1627686510c394d80d
Park jaemin <hungryshark4292@gmail.com>
2024-10-24 09:29:57 +0900

    notification-schemas 수정

--- notification/schemas.py
+++ notification/schemas.py
@@ -1,32 +1,38 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
+from enum import Enum
 from uuid import UUID
 from datetime import datetime
+
+
 
 class NotificationBase(BaseModel):
     title: str
     content: str
-    
+    grade: List[str]  
+    class_num: List[str]  
+
+
 class NotificationCreate(NotificationBase):
-     pass
+    pass
 
 class Notification(NotificationBase):
-  id : int
-  author_id: UUID
-  author_name: str
-  date: datetime
+    id: int
+    author_id: UUID
+    author_name: str
+    date: datetime
   
-  class Config:
-    from_attributes = True
-    
+    class Config:
+        from_attributes = True
+
 class NotificationSimple(BaseModel):
-     title : str
-     date: datetime
-     id : int
+    title: str
+    date: datetime
+    id: int
      
-     class Config:
-       from_attributes = True
-       
+    class Config:
+        from_attributes = True
        
 class NotificationUpdate(NotificationBase):
     pass
+
