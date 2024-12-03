7fd2e984a6ae55e5dac4e7996800f488879214a8
Park jaemin <hungryshark4292@gmail.com>
2024-12-03 09:46:29 +0900

    main 수정

--- main.py
+++ main.py
@@ -24,10 +24,9 @@ app.include_router(notification_router.router, tags=["공지"]) # 공지
 app.include_router(admin_router.router, tags=['어드민']) # 어드민 로그인 회원가입
 app.include_router(comments_router.router, tags=['공지사항 댓글'])
 app.include_router(user_mypage_router.router, tags=['유저 마이페이지'])
-app.include_router(auth_router.router, tags= ['토큰 인증'])
 app.include_router(homework_router.router, tags=['과제'])
 
-if __name__ == "__main__":
-  init_teacher_db()
-  import uvicorn
-  uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+# if __name__ == "__main__":
+#   init_teacher_db()
+#   import uvicorn
+#   uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
