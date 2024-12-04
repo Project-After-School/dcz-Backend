f4725baa7baac40c6c3f6d50bfb5aa66a27be4cb
김대운 <eodnseodns23@gmail.com>
2024-12-04 14:11:43 +0900

    tlqkf

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.include_router(comments_router.router, tags=['공지사항 댓글'])
 app.include_router(user_mypage_router.router, tags=['유저 마이페이지'])
 app.include_router(homework_router.router, tags=['과제'])
 
-# if __name__ == "__main__":
-#    init_teacher_db()
-#    import uvicorn
-#    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
+if __name__ == "__main__":
+    init_teacher_db()
+    import uvicorn
+    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
